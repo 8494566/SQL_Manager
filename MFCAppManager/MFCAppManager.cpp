5b10724e8543387f6746b0f748817fd869bbe7da
wanggaoliang <8494566@qq.com>
2020-07-23 15:47:27 +0800

    多字符集兼容

--- MFCAppManager/MFCAppManager.cpp
+++ MFCAppManager/MFCAppManager.cpp
@@ -69,7 +69,7 @@ BOOL CMFCAppManagerApp::InitInstance()
 	// 更改用于存储设置的注册表项
 	// TODO: 应适当修改该字符串，
 	// 例如修改为公司或组织名
-	SetRegistryKey(("应用程序向导生成的本地应用程序"));
+	SetRegistryKey("应用程序向导生成的本地应用程序");
 
 	CMFCAppManagerDlg dlg;
 	m_pMainWnd = &dlg;
